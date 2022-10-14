b9ebbd6371276768dd2f0f53f20379794d406acf
Kristine Johnson <100108249+kristinejohnson7@users.noreply.github.com>
2022-10-13 20:04:28 -0400

    Add quotes in testimonial section, mobile modifications

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,51 +1,83 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { AnimatePresence } from "framer-motion";
 import { Pagination, Navigation } from "swiper";
 import "./Testimonials.scss";
 import testimonialsContext from "../../context/testimonials";
 import Header from "../Header/Header";
+import Modal from "../Modal/Modal";
 import { Fade } from "react-awesome-reveal";
-import { LazyLoadComponent } from "react-lazy-load-image-component";
+import { ReactComponent as Quote } from "../../assets/quote-left.svg";
 
 export default function Testimonials() {
   const { testimonials } = useContext(testimonialsContext);
+  const [modalData, setModalData] = useState("");
+  const [isOpen, setIsOpen] = useState("");
+
+  const handleDisplayVideo = (e, video) => {
+    setModalData(video);
+    setIsOpen(true);
+  };
 
   return (
     <section id="testimonials" className="container">
-      <Header title="Testimonials" variant="primary" />
       <Fade>
-        <LazyLoadComponent>
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={30}
-            loop={true}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-            className="testimonySwiper"
-          >
-            {testimonials.map((testimonial) => (
-              <SwiperSlide key={testimonial.id}>
-                <div className="testimonialIframe">
-                  <iframe
-                    title="title"
-                    src={testimonial.videoSource}
-                    frameborder="0"
-                    webkitallowfullscreen
-                    mozallowfullscreen
-                    allowfullscreen
-                  ></iframe>
+        <Header title="Testimonials" variant="primary" />
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="testimonySwiper"
+        >
+          {testimonials.map((testimonial) => {
+            const { id, videoSource, title, quote } = testimonial;
+            return (
+              <SwiperSlide key={id} className="testimonyItem">
+                <Quote className="icon" />
+                <div className="text">
+                  <p>
+                    {quote}
+                    <span
+                      className="videoModal"
+                      onClick={(e) => handleDisplayVideo(e, videoSource)}
+                    >
+                      See full video testimony.
+                    </span>
+                  </p>
+                  <p className="name">{title}</p>
                 </div>
               </SwiperSlide>
-            ))}
-          </Swiper>
-        </LazyLoadComponent>
+            );
+          })}
+        </Swiper>
       </Fade>
+      <AnimatePresence initial={false} exitBeforeEnter={true}>
+        {isOpen && (
+          <Modal>
+            <div className="testimonialIframe">
+              <button className="btn" onClick={() => setIsOpen(false)}>
+                X
+              </button>
+              <iframe
+                title="title"
+                src={modalData}
+                frameborder="0"
+                webkitallowfullscreen
+                mozallowfullscreen
+                allowfullscreen
+              ></iframe>
+            </div>
+          </Modal>
+        )}
+      </AnimatePresence>
     </section>
   );
 }
